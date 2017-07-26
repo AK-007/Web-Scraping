74ed1b5154c7f05f59e339db44075f6486e2cdbc
Ayush Gupta <ayushkgupta007@gmail.com>
2017-07-26 14:55:19 +0530

    Delete qw.py

--- qw.py
+++
@@ -1,8 +0,0 @@
-import urllib2  
-from bs4 import BeautifulSoup
-quote_page = 'http://www.imdb.com/chart/top?ref_=nv_mv_250_6'
-page = urllib2.urlopen(quote_page)
-soup = BeautifulSoup(page, 'html.parser')
-name_box = soup.find('div', attrs={'class': 'lister'})
-name = name_box.text
-print name
